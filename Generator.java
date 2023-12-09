95b77438f6cb0574fdc917affe7d63d0703c1997
Oiya861028 <yubinxie222@gmail.com>
2023-12-08 21:26:15 -0500

    Remove the shuffle method due to lack of randomness

--- Generator.java
+++ Generator.java
@@ -27,40 +27,6 @@ public class Generator {
             System.out.println();
         }
     }
-    /* Shuffle not random enough. Discarded
-    public static int[][] shuffle(int[][] arr,int times){
-        //Comvert the two Dimensional arr into 1D so the shuffle can be performed in 1D
-        int[] shuffleDeck = new int[arr.length*arr[0].length];
-        int index = 0;
-        for(int i=0;i<arr.length;i++){
-            for(int j=0;j<arr[i].length;j++) {
-                shuffleDeck[index] = arr[i][j];
-                index++;
-            }
-        }
-        //shuffle for n times. Each shuffle will update the shuffleDeck[]
-        for(int n = 0; n <times; n++){
-            int[] temp = new int[shuffleDeck.length];
-            int k=0;
-            for(int i=0;i<temp.length/2;i++){
-                temp[k] = shuffleDeck[i];
-                temp[k+1] = shuffleDeck[shuffleDeck.length/2+i];
-                k+=2;
-            }
-            temp[temp.length-1] = 9;
-            shuffleDeck = temp;
-        }
-        //Convert the shuffledDeck back to arr
-        index =0;
-        for(int i=0;i<arr.length;i++){
-            for(int j=0;j<arr[i].length;j++) {
-                arr[i][j] = shuffleDeck[index];
-                index++;
-            }
-        }
-        return arr;
-    }
 
-     */
 
 }
