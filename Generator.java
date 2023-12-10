7d5cc843825b9596c6ca5f2216438cc24c2e5a5b
Oiya861028 <yubinxie222@gmail.com>
2023-12-10 16:55:56 -0500

    Add generate horizontal cell method

--- Generator.java
+++ Generator.java
@@ -1,9 +1,7 @@
 public class Generator {
     public static void main(String[] args){
-        printCell(generateOneCell());
-        printCell(generateOneCell());
-        printCell(generateOneCell());
-        printCell(generateOneCell());
+        printCell(generateThreeCells());
+        System.out.println();
     }
     public static int[][] generateOneCell(){
         int[][] arr = new int[3][3];
@@ -13,16 +11,54 @@ public class Generator {
                 int num = (int) (Math.random() * 9) +1;
                 while(generatedNumbers.toString().contains(Integer.toString(num))) num = (int) (Math.random() * 9) +1;
                 arr[i][j] = num;
-                generatedNumbers.append(Integer.toString(num));
-
+                generatedNumbers.append(num);
             }
         }
         return arr;
     }
+    public static int[][] generateThreeCells(){
+        int[][] arr = new int[3][9];
+        int[][] leftCell = generateOneCell();
+        //adding leftcell to the horizontal
+        for(int i=0;i<leftCell.length;i++){
+            System.arraycopy(leftCell[i], 0, arr[i], 0, 3);
+        }
+        //Creating the middle cell and adding to arr
+        StringBuilder middleCellTaken = new StringBuilder(); //use for checking the middle cell
+        StringBuilder rightCellTaken = new StringBuilder(); //use for checking the right cell
+        for(int i=0;i<arr.length;i++){
+            StringBuilder rowTakenNum = new StringBuilder(); //use for checking the rows
+            for(int n:leftCell[i]){
+                rowTakenNum.append(n);
+            }
+
+            for(int j=3;j<arr[i].length-3;j++){
+                int num = (int) (Math.random() * 9) +1;
+                while(rowTakenNum.toString().contains(Integer.toString(num)) || middleCellTaken.toString().contains(Integer.toString(num))){
+                    num = (int) (Math.random() * 9) +1;
+                }
+                arr[i][j] = num;
+                rowTakenNum.append(num);
+                middleCellTaken.append(num);
+            }
+            //Adding numbers to the right cell
+            for(int j=arr[i].length-3;j<arr[i].length;j++){
+                int num = (int) (Math.random() * 9) +1;
+                while(rowTakenNum.toString().contains(Integer.toString(num)) || rightCellTaken.toString().contains(Integer.toString(num))){
+                    num = (int) (Math.random() * 9) +1;
+                }
+                arr[i][j] = num;
+                rowTakenNum.append(num);
+                rightCellTaken.append(num);
+            }
+        }
+        return arr;
+
+    }
     public static void printCell(int[][] cell){
-        for(int i=0;i<3; i++){
-            for(int j=0;j<3;j++){
-                System.out.print(cell[i][j]+" ");
+        for (int[] ints : cell) {
+            for (int anInt : ints) {
+                System.out.print(anInt + " ");
             }
             System.out.println();
         }
